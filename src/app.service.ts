d3d7301ab201a824a46ff6a1a8f981968cec71c3
Rodrigo Joaquin Guzman <joaquin.guzman@ucse.net.ar>
2023-07-11 19:25:09 -0300

    con mssql

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,34 @@
 import { Injectable } from '@nestjs/common';
+require('tls').DEFAULT_MIN_VERSION = 'TLSv1';
+var sql = require('mssql');
+var config = {
+  user: 'devConceptos',
+  password: 'qwerty',
+  server: 'sgo-desarrollo',
+  database: 'GestionConceptos',
+
+  options: {
+    trustedConnection: true,
+    encrypt: false, // modifique a false para que no me de error de certificado
+    enableArithAbort: true,
+    trustServerCertificate: false,
+  },
+};
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'LLEGO AL SERVICIO_________________';
+  }
+
+  async newFormulario(data) {
+    try {
+      let consulta = 'select * from ContactosDocentes';
+      await sql.connect(config);
+      const result = await sql.query(consulta);
+      return result.recordsets[0];
+    } catch (error) {
+      return error;
+    }
   }
 }
